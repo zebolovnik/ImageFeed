ea5bbe9ef14e1f158f1693799c9467d65d5dd8a7
NikZebolov <zebolovnik@yandex.ru>
2024-09-29 14:59:45 +1000

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Nikolay Zebolov on 29.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
