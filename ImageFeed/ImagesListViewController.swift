6dfe747685753d6ead1eebf10ee39bbdd74857a1
NikZebolov <zebolovnik@yandex.ru>
2025-10-12 13:21:03 +1000

    - Rename ViewController.swift → ImageListViewController.swift - Rename storyboard scene ImagesListViewController → ImageList - Set view background to YP Background #1A1B22 - Add full-screen UITableView (constraints to Superview) - TableView background: YP White (alpha 50), separators removed - Add private outlet: @IBOutlet private var tableView: UITableView!
    Checks: storyboard outlet connected, build successful.

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ImagesListViewController: UIViewController {
+    @IBOutlet private var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
