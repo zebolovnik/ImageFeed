49a3482fd7cb2a80511984d031b126d2a599bc2a
NikZebolov <zebolovnik@yandex.ru>
2025-10-14 01:32:45 +1000

    - Created a new Swift file ImagesListCell.swift and added a new class ImagesListCell - Merged ImagesListViewController and ImagesListCell into a folder named ImagesList - Added reuseIdentifier to ImagesListCell (for table view cell reuse) - Used the ImagesListCell identifier for dequeueReusableCell - Replaced the cellForRowAt method implementation to use the custom ImagesListCell - Created IBOutlets for the gradient, date label, and like button - Implemented CAGradientLayer with 0.4 opacity

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,37 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Nikolay Zebolov on 13.10.2025.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var gradientView: UIView!
+    
+    private let gradientLayer = CAGradientLayer()
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.black.withAlphaComponent(0.4).cgColor
+        ]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+        gradientView.layer.insertSublayer(gradientLayer, at: 0)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = gradientView.bounds
+    }
+    
+}
--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -12,8 +12,12 @@ class ImagesListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
+                
+        tableView.rowHeight = 200
+    }
+    
+    func configCell(for cell: ImagesListCell) {
+        // пока пусто
     }
 }
 
@@ -24,7 +28,14 @@ extension ImagesListViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath) // 1
+        
+        guard let imageListCell = cell as? ImagesListCell else { // 2
+            return UITableViewCell()
+        }
+        
+        configCell(for: imageListCell) // 3
+        return imageListCell // 4
     }
     
 }
