974c2f86321b0ad476e7747357da237f8a00be6f
NikZebolov <zebolovnik@yandex.ru>
2025-10-18 22:08:19 +1000

    - Added SingleImageViewController and linked it in Main.storyboard - Added UIImageView with IBOutlet and constraints to layoutMargins - Set background color #1A1B22 and Content Mode Aspect Fit for test image - Marked as Initial View Controller for testing - Set TabBarController as Initial View Controller - Added Show segue from ImagesListCell to SingleImageViewController for image detail view

---
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Nikolay Zebolov on 18.10.2025.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+
+}
