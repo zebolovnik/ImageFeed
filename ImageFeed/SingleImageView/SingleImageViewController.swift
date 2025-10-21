86c9e6ec46724fe149a9e0a1dc11c205ff4b247d
NikZebolov <zebolovnik@yandex.ru>
2025-10-21 23:28:09 +1000

    Added zoom functionality for SingleImageViewController: - Embedded imageView into ScrollView - Set scrollView delegate - Configured min/max zoom scale - Implemented viewForZooming method

--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -10,19 +10,48 @@ import UIKit
 final class SingleImageViewController: UIViewController {
     var image: UIImage? {
         didSet {
-            guard isViewLoaded else { return }
+            guard isViewLoaded, let image else { return }
+            
             imageView.image = image
+            imageView.frame.size = image.size
         }
     }
     
-    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet private var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        
+        guard let image else { return }
         imageView.image = image
+        imageView.frame.size = image.size
+
     }
 
     @IBAction private func didTapBackButton() {
         dismiss(animated: true, completion: nil)
+    }
+    
+    // --- НОВОЕ: вспомогательная функция для выставления размера imageView и contentSize ---
+    private func updateImageViewFrame() {
+        guard isViewLoaded, let image = imageView.image else { return }
+        
+        // разрешаем imageView работать через frame (не через Auto Layout)
+        imageView.translatesAutoresizingMaskIntoConstraints = true
+        
+        // размер imageView = размер картинки
+        imageView.frame.size = image.size
+        scrollView.contentSize = image.size
+    }
+    
+}
+
+// --- НОВОЕ: UIScrollViewDelegate для зума ---
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
     }
 }
