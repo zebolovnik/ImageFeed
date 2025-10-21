9dd54bb15cb9c84cd238f01ad247411f7fdb9847
NikZebolov <zebolovnik@yandex.ru>
2025-10-21 23:59:05 +1000

    - Implement image rescaling and centering in ScrollView - Call rescaleAndCenterImageInScrollView after setting image in viewDidLoad and didSet

--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -14,6 +14,7 @@ final class SingleImageViewController: UIViewController {
             
             imageView.image = image
             imageView.frame.size = image.size
+            rescaleAndCenterImageInScrollView(image: image)
         }
     }
     
@@ -28,6 +29,7 @@ final class SingleImageViewController: UIViewController {
         guard let image else { return }
         imageView.image = image
         imageView.frame.size = image.size
+        rescaleAndCenterImageInScrollView(image: image)
 
     }
 
@@ -45,6 +47,23 @@ final class SingleImageViewController: UIViewController {
         // размер imageView = размер картинки
         imageView.frame.size = image.size
         scrollView.contentSize = image.size
+    }
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
     }
     
 }
