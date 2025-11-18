9c034c2b4d12c1a469f5a53eb21d8e777716940c
NikZebolov <zebolovnik@yandex.ru>
2025-11-18 22:49:03 +1000

    - test(webview): add WebViewPresenterSpy for testing - test(webview): add testViewControllerCallsViewDidLoad - test(webview): add WebViewViewControllerSpy - test(webview): add testPresenterCallsLoadRequest - test(webview): add testProgressVisibleWhenLessThenOne - test(webview): add testProgressHiddenWhenOne

---
+++ ImageFeedTests/WebViewTests/WebViewTests.swift
@@ -0,0 +1,69 @@
+//
+//  ImageFeedTests.swift
+//  ImageFeedTests
+//
+//  Created by Nikolay Zebolov on 18.11.2025.
+//
+
+@testable import ImageFeed
+import XCTest
+
+final class WebViewTests: XCTestCase {
+    func testViewControllerCallsViewDidLoad() {
+        //given
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(
+            withIdentifier: "WebViewViewController"
+        ) as! WebViewViewController
+        let presenter = WebViewPresenterSpy()
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        //when
+        _ = viewController.view
+        
+        //then
+        XCTAssertTrue(presenter.viewDidLoadCalled)
+    }
+    
+    func testPresenterCallsLoadRequest() {
+        //given
+        let viewController = WebViewViewControllerSpy()
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        //when
+        presenter.viewDidLoad()
+        
+        //then
+        XCTAssertTrue(viewController.loadRequestCalled)
+    }
+    
+    func testProgressVisibleWhenLessThenOne() {
+        //given
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        let progress: Float = 0.6
+        
+        //when
+        let shouldHideProgress = presenter.shouldHideProgress(for: progress)
+        
+        //then
+        XCTAssertFalse(shouldHideProgress)
+    }
+    
+    func testProgressHiddenWhenOne() {
+        //given
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        let progress: Float = 1.0
+        
+        //when
+        let shouldHideProgress = presenter.shouldHideProgress(for: progress)
+        
+        //then
+        XCTAssertTrue(shouldHideProgress)
+    }
+}
