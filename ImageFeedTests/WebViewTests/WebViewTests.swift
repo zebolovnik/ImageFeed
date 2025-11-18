0b65c71aad153c4d18524b8a8368431b7ffc41a9
NikZebolov <zebolovnik@yandex.ru>
2025-11-18 23:21:09 +1000

    - test(webview): add testAuthHelperAuthURL - test(webview): add testCodeFromURL - test(webview): complete WebViewTests coverage

--- ImageFeedTests/WebViewTests/WebViewTests.swift
+++ ImageFeedTests/WebViewTests/WebViewTests.swift
@@ -66,4 +66,38 @@ final class WebViewTests: XCTestCase {
         //then
         XCTAssertTrue(shouldHideProgress)
     }
+    
+    func testAuthHelperAuthURL() {
+        //given
+        let configuration = AuthConfiguration.standard
+        let authHelper = AuthHelper(configuration: configuration)
+        
+        //when
+        let url = authHelper.authUrl()
+        guard let urlString = url?.absoluteString else {
+            XCTFail("Auth URL is nil")
+            return
+        }
+        
+        //then
+        XCTAssertTrue(urlString.contains(configuration.authURLString))
+        XCTAssertTrue(urlString.contains(configuration.accessKey))
+        XCTAssertTrue(urlString.contains(configuration.redirectURI))
+        XCTAssertTrue(urlString.contains("code"))
+        XCTAssertTrue(urlString.contains(configuration.accessScope))
+    }
+    
+    func testCodeFromURL() {
+        //given
+        var urlComponents = URLComponents(string: "https://unsplash.com/oauth/authorize/native")!
+        urlComponents.queryItems = [URLQueryItem(name: "code", value: "test code")]
+        let url = urlComponents.url!
+        let authHelper = AuthHelper()
+        
+        //when
+        let code = authHelper.code(from: url)
+        
+        //then
+        XCTAssertEqual(code, "test code")
+    }
 }
